a7d61289b4039b601090ab45bc42d70a8c624de2
CsYakamoz <532609418@qq.com>
2020-07-29 15:15:10 +0800

    fix(switching): update the logic of searching handler/schema file

--- src/switching/index.ts
+++ src/switching/index.ts
@@ -2,11 +2,12 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import { showQuickPick, execCommandOnShell } from '../utility';
 import { get } from './jump_config';
+import { existsSync } from 'fs';
 
 const FileType = {
     NONE: 0,
     SCHEMA: 1,
-    HANDLER: 2
+    HANDLER: 2,
 };
 
 const SchemaPattern = /(.*)\/schema\/([0-9a-z_\.]+)(?:[\/a-z0-9_\.]*)/;
@@ -34,7 +35,16 @@ export default async function switching(uri: any) {
             ) as RegExpMatchArray;
 
             const apiName = path.basename(apiNameWithExt, '.js');
-            const command = `find ${prefix}/handler -type f | grep ${apiName}`;
+            const handlerPath = `${prefix}/handler`;
+
+            if (!existsSync(handlerPath)) {
+                await vscode.window.showInformationMessage(
+                    `no such path(${handlerPath})`
+                );
+                return;
+            }
+
+            const command = `find ${handlerPath} -type f | grep -E "^${handlerPath}/${apiName}\\b"`;
             await find(command)
                 .then(select(prefix + '/handler'))
                 .then(openFile);
@@ -47,7 +57,16 @@ export default async function switching(uri: any) {
                 HandlerPattern
             ) as RegExpMatchArray;
 
-            const command = `find ${prefix}/schema -type f | grep ${apiName}`;
+            const schemaPath = `${prefix}/schema`;
+
+            if (!existsSync(schemaPath)) {
+                await vscode.window.showInformationMessage(
+                    `no such path(${schemaPath})`
+                );
+                return;
+            }
+
+            const command = `find ${schemaPath} -type f | grep -E "^${schemaPath}/${apiName}\\b"`;
             await find(command)
                 .then(select(prefix + '/schema'))
                 .then(openFile);
@@ -61,20 +80,20 @@ export default async function switching(uri: any) {
 
 async function openFile(fsPath: string) {
     const config = get();
-    return vscode.workspace.openTextDocument(fsPath).then(doc =>
+    return vscode.workspace.openTextDocument(fsPath).then((doc) =>
         vscode.window.showTextDocument(doc, {
             preview: config.preview,
             viewColumn: config.viewColumn,
-            preserveFocus: config.preserveFocus
+            preserveFocus: config.preserveFocus,
         })
     );
 }
 
 async function find(command: string) {
     return execCommandOnShell(command)
-        .then(str => str.split('\n'))
-        .then(arr => arr.filter(_ => _ !== ''))
-        .then(arr => arr.sort((a, b) => a.length - b.length));
+        .then((str) => str.split('\n'))
+        .then((arr) => arr.filter((_) => _ !== ''))
+        .then((arr) => arr.sort((a, b) => a.length - b.length));
 }
 
 function getFileType(fsPath: string): number {
@@ -90,16 +109,16 @@ function getFileType(fsPath: string): number {
 }
 
 function select(prefix: string) {
-    return async function(list: string[]) {
+    return async function (list: string[]) {
         if (list.length === 1) {
             return list[0];
         }
 
         const config = get();
-        const relPathList = list.map(_ => path.relative(prefix, _));
+        const relPathList = list.map((_) => path.relative(prefix, _));
 
         if (config.defaultPoint !== undefined) {
-            const target = relPathList.find(_ => _ === config.defaultPoint);
+            const target = relPathList.find((_) => _ === config.defaultPoint);
             if (target !== undefined) {
                 return path.join(prefix, target);
             }
